8771756e9e8fe4cf996f119ec839a9a124903b7b
Adriano Felisberto <adriano.felisberto@ctis.com.br>
2020-07-07 11:04:16 -0300

    feat: should be add question

--- src/app/components/dynamic-form-question/dynamic-form-question.component.ts
+++ src/app/components/dynamic-form-question/dynamic-form-question.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { QuestionBase } from '../../models/question-base';
+import { FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-dynamic-form-question',
+  selector: 'app-question',
   templateUrl: './dynamic-form-question.component.html',
-  styleUrls: ['./dynamic-form-question.component.scss']
 })
-export class DynamicFormQuestionComponent implements OnInit {
+export class DynamicFormQuestionComponent {
+  @Input() question: QuestionBase<string>;
+  @Input() form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get isValid(): boolean {
+    return this.form.controls[this.question.key].valid;
   }
-
 }

066e8c9a59ad4b3e99b5b0b941c27cb51ec6b726
Adriano Felisberto <adriano.felisberto@ctis.com.br>
2020-07-07 11:16:05 -0300

    feat: should be add services in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 import { DynamicFormQuestionComponent } from './components/dynamic-form-question/dynamic-form-question.component';
+import { QuestionService } from './services/question.service';
+import { QuestionControlService } from './services/question-control.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,10 @@ import { DynamicFormQuestionComponent } from './components/dynamic-form-question
     BrowserModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    QuestionService,
+    QuestionControlService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
